c95339a0c55af69aeb8b42aabf9977f511df37b7
sryanprograms <98987115+sryanprograms@users.noreply.github.com>
2024-11-05 13:11:35 -0700

    Update automation.m

--- automation.m
+++ automation.m
@@ -11,7 +11,6 @@ switch key
       distance = brick.UltrasonicDist(4);
       color = brick.ColorCode(4);
       press = brick.TouchPressed(1); %change touch port to whatever we end up connecting it to
-      
       brick.MoveMotor('A', 50);
       brick.MoveMotor('D', 50);
     
@@ -22,26 +21,32 @@ switch key
         
       %move right if there is a wall to the left and a wall in front is touched  
       elseif(distance>=25 && touch==1)
+        pause(1);
         brick.MoveMotors('A', -50)
         brick.MoveMotors('D', 50)
         
       %move left if there is a wall to the right and a wall in front is touched  
-      elseif(distance<25 && touch==1)
+      elseif(distance<=25 && touch==1)
+        pause(1);        
         brick.MoveMotors('A', 50)
         brick.MoveMotors('D', -50)
 
       %once in pickup zone change to manual control
       elseif(color==2)
-        pause(0.1);
+        pause(1);
         switch key
           case 'uparrow'
+            pause(1);
             brick.MoveMotor('AD', -50);
           case 'downarrow'
+            pause(1);
             brick.MoveMotor('AD', 50);
           case 'leftarrow'
+            pause(1);
             brick.MoveMotor('A', 0);
             brick.MoveMotor('D', 50);
           case 'rightarrow'
+            pause(1)
             brick.MoveMotor('A', 50);
             brick.MoveMotor('D', 0);
           case 0
@@ -50,10 +55,13 @@ switch key
             brick.StopAllMotors;
             break;  % Uncomment if needed
           case 'a'
+            pause(1)
             brick.MoveMotor('B', 50);
           case 'b'
+            pause(1)
             brick.MoveMotor('B', -50);
-       end   
+        
+       end
    end
 end
 
