d87788e5032080cf2ea9dced78a3716b9a3aa35f
sryanprograms <98987115+sryanprograms@users.noreply.github.com>
2024-11-19 12:25:14 -0700

    Update automation(nogyro)-V2.m

--- automation(nogyro)-V2.m
+++ automation(nogyro)-V2.m
@@ -1,3 +1,6 @@
+% Initialize Gyro Sensor
+brick.GyroReset('1'); % Reset the gyro sensor to zero
+
 % Main Loop
 while ~taskCompleted
     % Kill Switch
@@ -11,12 +14,25 @@ while ~taskCompleted
     distance = brick.UltrasonicDist(4);  % Ultrasonic sensor (on left)
     color = brick.ColorCode(3);         % Color sensor
     press = brick.TouchPressed(2);      % Touch sensor (front)
+    currentAngle = brick.GyroAngle('1'); % Gyro sensor for angle
 
     % Autonomous Mode
     if ~manualMode
-        % Drive Forward
-        brick.MoveMotor('A', -DRIVE_SPEED);
-        brick.MoveMotor('D', -DRIVE_SPEED);
+        % Straight-Line Correction with Gyro Sensor
+        targetAngle = 0; % Target angle for straight-line movement
+        if currentAngle > targetAngle
+            % Adjust motors to correct veering to the right
+            brick.MoveMotor('A', -(DRIVE_SPEED - 10)); % Negative for forward
+            brick.MoveMotor('D', -(DRIVE_SPEED + 10));
+        elseif currentAngle < targetAngle
+            % Adjust motors to correct veering to the left
+            brick.MoveMotor('A', -(DRIVE_SPEED + 10));
+            brick.MoveMotor('D', -(DRIVE_SPEED - 10));
+        else
+            % Drive straight
+            brick.MoveMotor('A', -DRIVE_SPEED);
+            brick.MoveMotor('D', -DRIVE_SPEED);
+        end
 
         % Stop at Stop Signs (Red)
         if color == 5
@@ -43,34 +59,40 @@ while ~taskCompleted
             pause(0.5);
             if distance < 25
                 disp('Obstacle detected on the left. Turning right...');
-                % Turn right
-                brick.MoveMotorAngleRel('A', -TURN_SPEED, 360, 'D', TURN_SPEED, 360);
-                brick.WaitForMotor('A');
-                brick.WaitForMotor('D');
+                % Gyro-Based 90-degree Right Turn
+                targetTurn = currentAngle + 90; % Target angle after 90-degree turn
+                while brick.GyroAngle('1') < targetTurn
+                    brick.MoveMotor('A', TURN_SPEED); % Reverse logic: left motor forward
+                    brick.MoveMotor('D', -TURN_SPEED); % Right motor backward
+                end
+                brick.StopAllMotors();
             else
                 disp('Path clear on the left. Turning left...');
-                % Turn left
-                brick.MoveMotorAngleRel('A', TURN_SPEED, 360, 'D', -TURN_SPEED, 360);
-                brick.WaitForMotor('A');
-                brick.WaitForMotor('D');
+                % Gyro-Based 90-degree Left Turn
+                targetTurn = currentAngle - 90; % Target angle after 90-degree turn
+                while brick.GyroAngle('1') > targetTurn
+                    brick.MoveMotor('A', -TURN_SPEED); % Reverse logic: left motor backward
+                    brick.MoveMotor('D', TURN_SPEED);  % Right motor forward
+                end
+                brick.StopAllMotors();
             end
         end
     end
 
-    % Manual Mode (remains unchanged from previous example)
+    % Manual Mode (reversed directions for control)
     if manualMode
         switch key
-            case 'uparrow'
-                brick.MoveMotor('A', DRIVE_SPEED);
-                brick.MoveMotor('D', DRIVE_SPEED);
-            case 'downarrow'
+            case 'uparrow'  % Forward (negative due to reversed orientation)
                 brick.MoveMotor('A', -DRIVE_SPEED);
                 brick.MoveMotor('D', -DRIVE_SPEED);
-            case 'leftarrow'
-                brick.MoveMotor('A', 0);
-                brick.MoveMotor('D', DRIVE_SPEED);
-            case 'rightarrow'
+            case 'downarrow' % Backward (positive due to reversed orientation)
                 brick.MoveMotor('A', DRIVE_SPEED);
+                brick.MoveMotor('D', DRIVE_SPEED);
+            case 'leftarrow' % Turn left
+                brick.MoveMotor('A', 0);
+                brick.MoveMotor('D', -DRIVE_SPEED);
+            case 'rightarrow' % Turn right
+                brick.MoveMotor('A', -DRIVE_SPEED);
                 brick.MoveMotor('D', 0);
             case 'a'  % Open Arm
                 brick.MoveMotor('B', 50);
@@ -97,4 +119,3 @@ while ~taskCompleted
         end
     end
 end
-
