47d6f755d1e6353fdc5cbe6c33c037b1d75c2628
sryanprograms <98987115+sryanprograms@users.noreply.github.com>
2024-11-21 13:32:17 -0700

    Update automation(nogyro).m

--- automation(nogyro).m
+++ automation(nogyro).m
@@ -1,9 +1,9 @@
-%brick = ConnectBrick('VIRUS'); run this in command window before press run
+% brick = ConnectBrick('VIRUS'); run this in the command window before pressing run
 
 global key;
 InitKeyboard();
 
-%  set color sensor
+% Set color sensor
 brick.SetColorMode(3, 2);
 
 beginMoving = 1;
@@ -12,45 +12,78 @@ pickupDone = false;
 dropoffDone = false;
 
 while beginMoving
-    % Kill switch
-    if brick.TouchPressed(1)
-        brick.StopAllMotors();
-        disp('Kill switch pressed.');
-        break;
-    end
-    
-    % sensor ports
+    % Sensor ports
     distance = brick.UltrasonicDist(4);
     color = brick.ColorCode(3);
     press = brick.TouchPressed(2);
 
-    % autonomous
+    % Check for 'q' quick button or manual mode inputs
+    switch key
+        case 'q'  % Quick quit button
+            disp('Quick quit button pressed. Exiting...');
+            brick.StopAllMotors();
+            break;
+
+        case 'space'  % Switch to autonomous mode
+            disp('Switching to autonomous mode.');
+            manualMode = false;
+
+        % Manual control
+        case 'uparrow'
+            brick.MoveMotor('A', 50);
+            brick.MoveMotor('D', 50);
+        case 'downarrow'
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('D', -50);
+        case 'leftarrow'
+            brick.MoveMotor('A', 0);
+            brick.MoveMotor('D', 50);
+        case 'rightarrow'
+            brick.MoveMotor('A', 50);
+            brick.MoveMotor('D', 0);
+        case 'a'  % Open mechanical arm
+            brick.MoveMotor('B', 50);
+            pause(1);
+            brick.StopMotor('B');
+        case 'b'  % Close mechanical arm
+            brick.MoveMotor('B', -50);
+            pause(1);
+            brick.StopMotor('B');
+    end
+
+    % Autonomous mode
     if ~manualMode
-        % forward
+        % Forward motion
         brick.MoveMotor('A', -50);
         brick.MoveMotor('D', -50);
-        
-        % stop sign
+
+        % Stop sign
         if color == 5
             brick.StopAllMotors();
             pause(5);  % Wait for 5 seconds
-        % pickup zone
+
+        % Pickup zone
         elseif color == 2 && ~pickupDone
             brick.StopAllMotors();
+            disp('Pickup zone reached. Switching to manual mode.');
             manualMode = true;
-        % drop-off zone
+
+        % Drop-off zone
         elseif color == 3 && pickupDone && ~dropoffDone
             brick.StopAllMotors();
+            disp('Drop-off zone reached. Switching to manual mode.');
             manualMode = true;
-        % follow walls 
+
+        % Wall-following logic
         elseif distance <= 25 && press == 1
-            % wall on the left turn right
+            % Wall on the left, turn right
             pause(0.5);
             brick.MoveMotorAngleRel('A', -50, 180, 'D', 50, 180);  
             brick.WaitForMotor('A');
             brick.WaitForMotor('D');
+
         elseif distance > 25 && press == 1
-            % wall on the right turn left
+            % Wall on the right, turn left
             pause(0.5);
             brick.MoveMotorAngleRel('A', 50, 180, 'D', -50, 180); 
             brick.WaitForMotor('A');
@@ -58,46 +91,10 @@ while beginMoving
         end
     end
 
-    % manual 
-    if manualMode
-        switch key
-            case 'uparrow'
-                brick.MoveMotor('A', 50);
-                brick.MoveMotor('D', 50);
-            case 'downarrow'
-                brick.MoveMotor('A', -50);
-                brick.MoveMotor('D', -50);
-            case 'leftarrow'
-                brick.MoveMotor('A', 0);
-                brick.MoveMotor('D', 50);
-            case 'rightarrow'
-                brick.MoveMotor('A', 50);
-                brick.MoveMotor('D', 0);
-            case 'a'  % Open mechanical arm
-                brick.MoveMotor('B', 50);
-                pause(1);
-                brick.StopMotor('B');
-            case 'b'  % Close mechanical arm
-                brick.MoveMotor('B', -50);
-                pause(1);
-                brick.StopMotor('B');
-            case 'space'
-                % switch to autonomous
-                manualMode = false;
-                if color == 2
-                    pickupDone = true;
-                elseif color == 3
-                    dropoffDone = true;
-                end
-            case 'q'  % quit (kill switch for keyboard)
-                brick.StopAllMotors();
-                break;
-        end
-    end
-
     % Check if the task is complete
     if dropoffDone && color == 4
         brick.StopAllMotors();
+        disp('Task complete. Exiting...');
         break;
     end
 end
